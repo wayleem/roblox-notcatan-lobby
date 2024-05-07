1bf7f75fd0586bc1aa1104bcb005bcdc543c5e29
william <wayleemh@gmail.com>
2024-05-06 20:35:44 -0400

    flush server store changes to local store

--- src/client/main.client.ts
+++ src/client/main.client.ts
@@ -1,3 +1,12 @@
+import { ReplicatedStorage } from "@rbxts/services";
+import { MyActions } from "shared/actions";
 import { makeHello } from "shared/module";
+import { local_store } from "./local_store";
+
+const serverToClientEvent = ReplicatedStorage.WaitForChild("UpdateClientEvent") as RemoteEvent;
+serverToClientEvent.OnClientEvent.Connect((action: MyActions<any>) => {
+	//print("Received action from server: ", action);
+	local_store.dispatch(action);
+});
 
 print(makeHello("main.client.ts"));
