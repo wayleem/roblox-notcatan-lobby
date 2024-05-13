5d6cf3085875b749bc1958b0ea16ebd0109c08f4
william <wayleemh@gmail.com>
2024-05-13 05:26:10 -0400

    test roact mounting

--- src/client/main.client.ts
+++ src/client/main.client.ts
@@ -1,9 +1,14 @@
-import { ReplicatedStorage } from "@rbxts/services";
+import { Players, ReplicatedStorage } from "@rbxts/services";
 import { MyActions } from "shared/actions";
 import { makeHello } from "shared/module";
 import { local_store } from "./local_store";
+import Roact from "@rbxts/roact";
 
 const serverToClientEvent = ReplicatedStorage.WaitForChild("UpdateClientEvent") as RemoteEvent;
+
+const playerGui = Players.LocalPlayer.WaitForChild("PlayerGui") as PlayerGui;
+const screenGui = playerGui.WaitForChild("ScreenGui") as ScreenGui;
+
 serverToClientEvent.OnClientEvent.Connect((action: MyActions<any>) => {
 	//print("Received action from server: ", action);
 	local_store.dispatch(action);
